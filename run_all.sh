d2fad4253033e934fe76f5616f7a26111326c5e5
Arthur Ferreira Ely <arthur.ely78@gmail.com>
2024-08-10 16:51:07 -0300

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -12,11 +12,11 @@ cd orgb_progs
 # Executa todos os arquivos que são executáveis
 for file in *; do
     if [ -x "$file" ] && [ ! -d "$file" ]; then
-        echo "Executando $file..."
-        ./"$file"
+        echo "Executando $file com gem5..."
+        ./gem5 orgb_configs/simulate.py run-benchmark -c "orgb_progs/$file"
         echo "$file executado."
         echo ""
     fi
 done
 
-echo "Todos os executáveis foram executados."
+echo "Todos os executáveis foram executados com gem5."
