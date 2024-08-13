0fa336fe044dfa1a7a94cd8fb6710e5206614a44
Cauê Scotti <caue.scotti@gmail.com>
2024-08-12 23:35:12 -0300

    up primos

---
+++ primos/primos.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    int primos[500];
+    int ind = 0, div = 2, primo = 1;
+    
+
+    for (int i = 3; i < 1000; i++){
+        
+        while (primo && div != i){
+            if (i % div == 0)
+                primo = 0;
+            div++;
+        }
+
+        if (primo){
+            primos[ind] = i;
+            ind++;
+        }
+        div = 2;
+        primo = 1;
+    }
+
+    // for (int i = 0; i < ind; i++)
+    //     printf("%d ", primos[i]);
+
+    return 0;
+}
